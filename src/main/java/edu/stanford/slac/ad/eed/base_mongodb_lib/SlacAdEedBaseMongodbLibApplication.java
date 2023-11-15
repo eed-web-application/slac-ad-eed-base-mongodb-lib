0ad1cf701dfca10c2027b5460d54790d34a69b0b
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-11-14 19:02:21 -0800

    first commit

---
+++ src/main/java/edu/stanford/slac/ad/eed/base_mongodb_lib/SlacAdEedBaseMongodbLibApplication.java
@@ -0,0 +1,13 @@
+package edu.stanford.slac.ad.eed.base_mongodb_lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlacAdEedBaseMongodbLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlacAdEedBaseMongodbLibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/stanford/slac/ad/eed/base_mongodb_lib/SlacAdEedBaseMongodbLibApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.stanford.slac.ad.eed.base_mongodb_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlacAdEedBaseMongodbLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
