732486ee9a8146d6b3b6b9d942efc1f28aa7e921
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-11-16 13:53:04 -0800

    fix for builder

--- src/main/java/edu/stanford/slac/ad/eed/base_mongodb_lib/utility/InitAuthorizationIndex.java
+++ src/main/java/edu/stanford/slac/ad/eed/base_mongodb_lib/utility/InitAuthorizationIndex.java
@@ -2,15 +2,16 @@ package edu.stanford.slac.ad.eed.base_mongodb_lib.utility;
 
 import edu.stanford.slac.ad.eed.baselib.model.Authorization;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.index.Index;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
+@Builder
 @AllArgsConstructor
 public class InitAuthorizationIndex extends MongoDDLOps {
     private final MongoTemplate mongoTemplate;
-    private final MongoMappingContext mongoMappingContext;
 
     public void updateIndex() {
         MongoDDLOps.createIndex(
