b0642eb1d238adb2977071a6def2437cb2724335
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-01-31 12:00:20 -0800

    add comments

--- src/main/java/edu/stanford/slac/ad/eed/base_mongodb_lib/repository/AuthorizationRepository.java
+++ src/main/java/edu/stanford/slac/ad/eed/base_mongodb_lib/repository/AuthorizationRepository.java
@@ -9,16 +9,66 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AuthorizationRepository extends MongoRepository<Authorization, String> {
+    /**
+     * Find all authorizations for a given owner
+     * @param owner owner
+     * @return list of authorizations
+     */
     Optional<Authorization> findByOwnerIsAndResourceIsAndAuthorizationTypeIs(String owner, String resource, Integer authorizationType);
+    /**
+     * Find all authorizations for a given owner
+     * @param owner owner
+     * @return list of authorizations
+     */
     @Query("{ 'owner' : '?0', $or: [{'resource' : '?1', 'authorizationType' : { '$gte' : ?2}},{'authorizationType' : 2, resource:'*'}]}")
     Optional<Authorization> findByOwnerIsAndResourceIsAndAuthorizationTypeIsGreaterThanEqual(String owner, String resource, Integer authorizationType);
+    /**
+     * Find all authorizations for a given resource
+     * @param resource owner
+     * @return list of authorizations
+     */
     List<Authorization> findByResourceIs(String resource);
+    /**
+     * Find all authorizations for a given resource and authorization type
+     * @param resource owner
+     * @param authorizationType authorization type
+     * @return list of authorizations
+     */
     List<Authorization> findByResourceIsAndAuthorizationTypeIsGreaterThanEqual(String resource, Integer authorizationType);
+    /**
+     * Find all authorizations for a given resource and authorization type
+     * @param resource owner
+     * @param authorizationType authorization type
+     * @return list of authorizations
+     */
     List<Authorization> findByResourceIsAndAuthorizationTypeIsGreaterThanEqualAndOwnerTypeIs(String resource, Integer authorizationType, AuthorizationOwnerType ownerType);
-    //@Query("{ 'owner' : '?0', $or: [{'authorizationType' : { '$gte' : '?1'}, 'resource' : { $regex : \"?2\"}}, {'authorizationType' : 2, resource:'*'}]}")
+    /**
+     * Find all authorizations for a given resource and authorization type
+     * @param resource owner
+     * @param authorizationType authorization type
+     * @return list of authorizations
+     */
     List<Authorization> findByOwnerAndOwnerTypeAndAuthorizationTypeIsGreaterThanEqualAndResourceStartingWith(String owner, AuthorizationOwnerType ownerType, Integer authorizationType, String resource);
+    /**
+     * Delete all authorizations for a given owner, resource and authorization type
+     * @param owner owner
+     * @param resource authorization type
+     * @param authorizationType owner type
+     */
     void deleteByOwnerIsAndResourceIsAndAuthorizationTypeIs(String owner, String resource, Integer authorizationType);
+    /**
+     * Delete all authorizations for a given resource prefix
+     * @param resourcePrefix resource prefix
+     */
     void deleteAllByResourceStartingWith(String resourcePrefix);
+    /**
+     * Delete all authorizations for a given resource
+     * @param resource resource
+     */
     void deleteAllByResourceIs(String resource);
+    /**
+     * Delete all authorizations for a given owner
+     * @param owner owner
+     */
     void deleteAllByOwnerIs(String owner);
 }
