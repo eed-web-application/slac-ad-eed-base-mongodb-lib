e1c5e43d92c49c28129ee0b23d44e851f9b95ced
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-07-02 10:24:01 -0700

    fixes

--- src/main/java/edu/stanford/slac/ad/eed/base_mongodb_lib/service/ModelHistoryServiceImpl.java
+++ src/main/java/edu/stanford/slac/ad/eed/base_mongodb_lib/service/ModelHistoryServiceImpl.java
@@ -5,6 +5,7 @@ import edu.stanford.slac.ad.eed.baselib.api.v1.dto.ModelChangesHistoryDTO;
 import edu.stanford.slac.ad.eed.baselib.api.v1.mapper.ModelChangeMapper;
 import edu.stanford.slac.ad.eed.baselib.service.ModelHistoryService;
 import lombok.AllArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import static edu.stanford.slac.ad.eed.baselib.exception.Utility.wrapCatch;
 
 @Service
 @AllArgsConstructor
+@ConditionalOnProperty(name = "edu.stanford.slac.ad.eed.baselib.enable-model-change-history", havingValue = "true", matchIfMissing = false)
 public class ModelHistoryServiceImpl extends ModelHistoryService {
     private final ModelChangeMapper modelChangeMapper;
     private final ModelHistoryRepository modelHistoryRepository;
