b210fd688ee87aacf207de688e647baa6872beed
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-07-03 15:29:59 -0700

    fixes all tests

--- src/main/java/edu/stanford/slac/ad/eed/base_mongodb_lib/service/ModelHistoryServiceImpl.java
+++ src/main/java/edu/stanford/slac/ad/eed/base_mongodb_lib/service/ModelHistoryServiceImpl.java
@@ -1,17 +1,26 @@
 package edu.stanford.slac.ad.eed.base_mongodb_lib.service;
 
 import edu.stanford.slac.ad.eed.base_mongodb_lib.repository.ModelHistoryRepository;
+import edu.stanford.slac.ad.eed.baselib.api.v1.dto.ModelChangeDTO;
 import edu.stanford.slac.ad.eed.baselib.api.v1.dto.ModelChangesHistoryDTO;
 import edu.stanford.slac.ad.eed.baselib.api.v1.mapper.ModelChangeMapper;
 import edu.stanford.slac.ad.eed.baselib.service.ModelHistoryService;
 import lombok.AllArgsConstructor;
+import org.javers.core.Changes;
+import org.javers.core.Javers;
+import org.javers.core.commit.CommitId;
+import org.javers.core.diff.Change;
+import org.javers.core.diff.Diff;
+import org.javers.core.diff.changetype.PropertyChange;
+import org.javers.core.metamodel.object.CdoSnapshot;
+import org.javers.repository.jql.QueryBuilder;
+import org.javers.shadow.Shadow;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import static edu.stanford.slac.ad.eed.baselib.exception.Utility.wrapCatch;
 
@@ -19,18 +28,104 @@ import static edu.stanford.slac.ad.eed.baselib.exception.Utility.wrapCatch;
 @AllArgsConstructor
 @ConditionalOnProperty(name = "edu.stanford.slac.ad.eed.baselib.enable-model-change-history", havingValue = "true", matchIfMissing = false)
 public class ModelHistoryServiceImpl extends ModelHistoryService {
+    private final Javers javers;
     private final ModelChangeMapper modelChangeMapper;
     private final ModelHistoryRepository modelHistoryRepository;
 
     @Override
-    public List<ModelChangesHistoryDTO> findChangesByModelId(String modelId) {
-        return wrapCatch
-                (
-                        () -> modelHistoryRepository.findAllByModelId(modelId),
-                        -1
+    public <T> List<ModelChangesHistoryDTO> findChangesByModelId(Class<T> modelClazz, String modelId) {
+        List<CdoSnapshot> snapshots = getSnapshotById(modelClazz, modelId);
+        Changes changes = getChangesById(modelClazz, modelId);
+        return snapshots.stream()
+                .map(snapshot ->
+                        ModelChangesHistoryDTO.builder()
+                                .modelId(snapshot.getCommitMetadata().getId().toString())
+                                .modelId(modelId)
+                                .changes(extractChanges(changes, snapshot.getCommitId()))
+                                .createdDate(snapshot.getCommitMetadata().getCommitDate())
+                                .createdBy(snapshot.getCommitMetadata().getAuthor())
+                                .build()
                 )
-                .stream()
-                .map(modelChangeMapper::toDTO)
                 .toList();
+    }
+
+    @Override
+    public <T> List<T> findModelChangesByModelId(Class<T> modelClazz, String modelId) {
+        List<Shadow<T>> snapshotList = getShadowsById(modelClazz, modelId);
+        var result = snapshotList
+                .stream()
+                .sorted
+                        (
+                                Comparator.comparing
+                                        (
+                                                shadow -> shadow.getCommitMetadata().getCommitDate(),
+                                                Comparator.reverseOrder()
+                                        )
+                        )
+                .map(
+                        shadow -> shadow.get()
+                )
+                .toList();
+        return result;
+    }
+
+    /**
+     * Get all snapshots of a model by its id
+     *
+     * @param modelClazz model class
+     * @param modelId    model id
+     * @param <T>        model type
+     * @return list of snapshots
+     */
+    private <T> List<CdoSnapshot> getSnapshotById(Class<T> modelClazz, String modelId) {
+        return javers.findSnapshots(QueryBuilder.byInstanceId(modelId, modelClazz).build());
+    }
+
+    private <T> List<Shadow<T>> getShadowsById(Class<T> modelClazz, String modelId) {
+        return javers.findShadows(
+                QueryBuilder
+                        .byInstanceId(modelId, modelClazz)
+                        .build()
+        );
+    }
+
+    /**
+     * Get all changes of a model by its id
+     *
+     * @param modelClazz model class
+     * @param modelId    model id
+     * @param <T>        model type
+     * @return list of changes
+     */
+    private <T> Changes getChangesById(Class<T> modelClazz, String modelId) {
+        return javers.findChanges(QueryBuilder.byInstanceId(modelId, modelClazz).build());
+    }
+
+    /**
+     * Get all changes of a model by its id
+     */
+    private <T> List<ModelChangeDTO> extractChanges(Changes changes, CommitId commitId) {
+        var result = changes.stream()
+                .filter(
+                        change -> {
+                            boolean selected = change instanceof PropertyChange;
+                            selected = selected && change.getCommitMetadata().isPresent();
+                            selected = selected && change.getCommitMetadata().get().getId().equals(commitId);
+                            return selected;
+                        }
+                )
+                .map(change -> {
+                    PropertyChange propertyChange = (PropertyChange) change;
+                    return ModelChangeDTO.builder()
+                            .fieldName(propertyChange.getPropertyName())
+                            .oldValue(propertyChange.getLeft() != null ? propertyChange.getLeft() : null)
+                            .newValue(propertyChange.getRight() != null ? propertyChange.getRight(): null)
+                            .build();
+
+                })
+                .toList();
+        result = new LinkedList<>(result); // reverse the order of the list
+        Collections.reverse(result);
+        return result;
     }
 }
--- src/test/java/edu/stanford/slac/ad/eed/base_mongodb_lib/service/ModelHistoryServiceTest.java
+++ src/test/java/edu/stanford/slac/ad/eed/base_mongodb_lib/service/ModelHistoryServiceTest.java
@@ -6,6 +6,8 @@ import edu.stanford.slac.ad.eed.baselib.api.v1.dto.ModelChangeDTO;
 import edu.stanford.slac.ad.eed.baselib.config.AppProperties;
 import edu.stanford.slac.ad.eed.baselib.model.ModelChangesHistory;
 import edu.stanford.slac.ad.eed.baselib.service.ModelHistoryService;
+import org.assertj.core.api.Condition;
+import org.bson.Document;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -14,11 +16,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.ldap.repository.config.EnableLdapRepositories;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.util.CollectionUtils;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.*;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -52,6 +60,8 @@ public class ModelHistoryServiceTest {
     public void preTest() {
         mongoTemplate.remove(new Query(), ModelChangesHistory.class);
         mongoTemplate.remove(new Query(), TestChangeModelPrimitive.class);
+        mongoTemplate.getDb().getCollection("jv_snapshots").deleteMany(new Document());
+        mongoTemplate.getDb().getCollection("jv_head_id").deleteMany(new Document());
     }
 
     @Test
@@ -65,8 +75,17 @@ public class ModelHistoryServiceTest {
                                 .build()
                 )
         );
-        var listOfChanges = assertDoesNotThrow(() -> modelHistoryService.findChangesByModelId(savedTestModel.getId()));
-        assertThat(listOfChanges).isEmpty();
+        var listOfChanges = assertDoesNotThrow(() -> modelHistoryService.findChangesByModelId(TestChangeModelPrimitive.class, savedTestModel.getId()));
+        assertThat(listOfChanges).hasSize(1);
+        assertThat(listOfChanges.get(0).changes()).isNotEmpty();
+        assertThat(listOfChanges.get(0).changes())
+                .extracting(ModelChangeDTO::newValue)
+                .contains("true", "string1");
+
+        var modelStateChanges = assertDoesNotThrow(() -> modelHistoryService.findModelChangesByModelId(TestChangeModelPrimitive.class, savedTestModel.getId()));
+        assertThat(modelStateChanges).isNotEmpty().hasSize(1);
+        assertThat(modelStateChanges.get(0).getBoolField1()).isTrue();
+        assertThat(modelStateChanges.get(0).getStringField1()).isEqualTo("string1");
     }
 
     @Test
@@ -92,8 +111,8 @@ public class ModelHistoryServiceTest {
                                 .build()
                 )
         );
-        var listOfChanges = assertDoesNotThrow(() -> modelHistoryService.findChangesByModelId(savedTestModel.getId()));
-        assertThat(listOfChanges).isNotEmpty().hasSize(2);
+        var listOfChanges = assertDoesNotThrow(() -> modelHistoryService.findChangesByModelId(TestChangeModelPrimitive.class, savedTestModel.getId()));
+        assertThat(listOfChanges).isNotEmpty().hasSize(3);
         assertThat(listOfChanges.get(0).changes()).hasSize(1);
         assertThat(listOfChanges.get(0).changes())
                 .extracting(ModelChangeDTO::newValue)
@@ -102,6 +121,15 @@ public class ModelHistoryServiceTest {
         assertThat(listOfChanges.get(1).changes())
                 .extracting(ModelChangeDTO::newValue)
                 .contains("false", "string2");
+
+        var modelStateChanges = assertDoesNotThrow(() -> modelHistoryService.findModelChangesByModelId(TestChangeModelPrimitive.class, savedTestModel.getId()));
+        assertThat(modelStateChanges).isNotEmpty().hasSize(3);
+        assertThat(modelStateChanges.get(0).getBoolField1()).isTrue();
+        assertThat(modelStateChanges.get(0).getStringField1()).isEqualTo("string2");
+        assertThat(modelStateChanges.get(1).getBoolField1()).isFalse();
+        assertThat(modelStateChanges.get(1).getStringField1()).isEqualTo("string2");
+        assertThat(modelStateChanges.get(2).getBoolField1()).isNull();
+        assertThat(modelStateChanges.get(2).getStringField1()).isNull();
 
     }
 
@@ -129,7 +157,7 @@ public class ModelHistoryServiceTest {
                                 )
                         );
 
-                        var listOfChanges = assertDoesNotThrow(() -> modelHistoryService.findChangesByModelId(savedTestModel.getId()));
+                        var listOfChanges = assertDoesNotThrow(() -> modelHistoryService.findChangesByModelId(TestChangeModelPrimitive.class, savedTestModel.getId()));
                         assertThat(listOfChanges).isNotEmpty().hasSize(1);
                         assertThat(listOfChanges.get(0).changes()).hasSize(1);
                         assertThat(listOfChanges.get(0).changes())
@@ -166,8 +194,8 @@ public class ModelHistoryServiceTest {
                 )
         );
         // verify all fields
-        var listOfChanges = assertDoesNotThrow(() -> modelHistoryService.findChangesByModelId(savedTestModel.getId()));
-        assertThat(listOfChanges).isNotEmpty().hasSize(1);
+        var listOfChanges = assertDoesNotThrow(() -> modelHistoryService.findChangesByModelId(TestChangeModelPrimitive.class, savedTestModel.getId()));
+        assertThat(listOfChanges).isNotEmpty().hasSize(2);
         assertThat(listOfChanges.get(0).changes()).hasSize(8);
         assertThat(listOfChanges.get(0).changes())
                 .extracting(ModelChangeDTO::fieldName)
@@ -192,18 +220,18 @@ public class ModelHistoryServiceTest {
                 )
         );
         // verify all fields
-        var listOfChanges = assertDoesNotThrow(() -> modelHistoryService.findChangesByModelId(savedTestModel.getId()));
-        assertThat(listOfChanges).isNotEmpty().hasSize(1);
+        var listOfChanges = assertDoesNotThrow(() -> modelHistoryService.findChangesByModelId(TestChangeModelArray.class, savedTestModel.getId()));
+        assertThat(listOfChanges).isNotEmpty().hasSize(2);
         assertThat(listOfChanges.get(0).changes()).hasSize(2);
         assertThat(listOfChanges.get(0).changes())
                 .extracting(ModelChangeDTO::fieldName)
                 .contains("stringField1", "boolField1");
         assertThat(listOfChanges.get(0).changes())
-                .extracting(ModelChangeDTO::newValue)
-                .contains("[string2, string3]", "[false, true]");
-        assertThat(listOfChanges.get(0).changes())
-                .extracting(ModelChangeDTO::oldValue)
-                .contains("[string1, string2]", "[true, false]");
+                .areAtLeastOne(new ArrayCondition(new Object[]{false, true}, true))
+                .areAtLeastOne(new ArrayCondition(new Object[]{"string2", "string3"}, true));
+        assertThat(listOfChanges.get(1).changes())
+                .areAtLeastOne(new ArrayCondition(new Object[]{true, false}, true))
+                .areAtLeastOne(new ArrayCondition(new Object[]{"string1", "string2"}, true));
     }
 
     @Test
@@ -224,18 +252,18 @@ public class ModelHistoryServiceTest {
                 )
         );
         // verify all fields
-        var listOfChanges = assertDoesNotThrow(() -> modelHistoryService.findChangesByModelId(savedTestModel.getId()));
-        assertThat(listOfChanges).isNotEmpty().hasSize(1);
+        var listOfChanges = assertDoesNotThrow(() -> modelHistoryService.findChangesByModelId(TestChangeModelList.class, savedTestModel.getId()));
+        assertThat(listOfChanges).isNotEmpty().hasSize(2);
         assertThat(listOfChanges.get(0).changes()).hasSize(2);
         assertThat(listOfChanges.get(0).changes())
                 .extracting(ModelChangeDTO::fieldName)
                 .contains("stringField1", "boolField1");
         assertThat(listOfChanges.get(0).changes())
-                .extracting(ModelChangeDTO::newValue)
-                .contains("[string2, string3]", "[false, true]");
-        assertThat(listOfChanges.get(0).changes())
-                .extracting(ModelChangeDTO::oldValue)
-                .contains("[string1, string2]", "[true, false]");
+                .areAtLeastOne(new ListCondition(List.of(false, true), true))
+                .areAtLeastOne(new ListCondition(List.of("string2", "string3"), true));
+        assertThat(listOfChanges.get(1).changes())
+                .areAtLeastOne(new ListCondition(List.of(true, false), true))
+                .areAtLeastOne(new ListCondition(List.of("string1", "string2"), true));
     }
 
 
@@ -256,16 +284,18 @@ public class ModelHistoryServiceTest {
                 )
         );
         // verify all fields
-        var listOfChanges = assertDoesNotThrow(() -> modelHistoryService.findChangesByModelId(savedTestModel.getId()));
-        assertThat(listOfChanges).isNotEmpty().hasSize(1);
+        var listOfChanges = assertDoesNotThrow(() -> modelHistoryService.findChangesByModelId(TestChangeModelMixed.class, savedTestModel.getId()));
+        assertThat(listOfChanges).isNotEmpty().hasSize(2);
         assertThat(listOfChanges.get(0).changes()).hasSize(3);
         assertThat(listOfChanges.get(0).changes())
                 .extracting(ModelChangeDTO::fieldName)
                 .contains("stringField1", "listBoolField1", "listStringField");
         assertThat(listOfChanges.get(0).changes())
                 .extracting(ModelChangeDTO::newValue)
-                .contains("string1", "[true, true]", "[string1, string2]");
-
+                .contains("string1");
+        assertThat(listOfChanges.get(0).changes())
+                .areAtLeastOne(new ListCondition(List.of(true, true), true))
+                .areAtLeastOne(new ListCondition(List.of("string1", "string2"), true));
         // another update
         var anotherUpdatedTestModel = assertDoesNotThrow(() -> testChangeModelMixedRepository.save(
                         savedTestModel.toBuilder()
@@ -276,22 +306,101 @@ public class ModelHistoryServiceTest {
                 )
         );
         // verify all fields
-        listOfChanges = assertDoesNotThrow(() -> modelHistoryService.findChangesByModelId(savedTestModel.getId()));
-        assertThat(listOfChanges).isNotEmpty().hasSize(2);
+        listOfChanges = assertDoesNotThrow(() -> modelHistoryService.findChangesByModelId(TestChangeModelMixed.class, savedTestModel.getId()));
+        assertThat(listOfChanges).isNotEmpty().hasSize(3);
         assertThat(listOfChanges.get(0).changes()).hasSize(3);
         assertThat(listOfChanges.get(0).changes())
                 .extracting(ModelChangeDTO::fieldName)
                 .contains("stringField1", "listBoolField1", "listStringField");
         assertThat(listOfChanges.get(0).changes())
                 .extracting(ModelChangeDTO::newValue)
-                .contains("string2", "[true]", "[string5, new-string1, new-string3]");
+                .contains("string2");
+        assertThat(listOfChanges.get(0).changes())
+                .areAtLeastOne(new ListCondition(List.of(true), true))
+                .areAtLeastOne(new ListCondition(List.of("string5", "new-string1", "new-string3"), true));
         assertThat(listOfChanges.get(1).changes()).hasSize(3);
         assertThat(listOfChanges.get(1).changes())
                 .extracting(ModelChangeDTO::fieldName)
                 .contains("stringField1", "listBoolField1", "listStringField");
         assertThat(listOfChanges.get(1).changes())
                 .extracting(ModelChangeDTO::newValue)
-                .contains("string1", "[true, true]", "[string1, string2]");
+                .contains("string1");
+        assertThat(listOfChanges.get(1).changes())
+                .areAtLeastOne(new ListCondition(List.of(true, true), true))
+                .areAtLeastOne(new ListCondition(List.of("string1", "string2"), true));
 
+    }
+
+    public class ListCondition extends Condition<ModelChangeDTO> {
+        private final List<Object> expectedValues;
+        private final boolean onNewValue;
+
+        public ListCondition(List<Object> expectedValues, boolean onNewValue) {
+            this.expectedValues = expectedValues;
+            this.onNewValue = onNewValue;
+        }
+
+        @Override
+        public boolean matches(ModelChangeDTO modelChangeDTO) {
+            if (onNewValue) {
+                if (modelChangeDTO.newValue() instanceof List) {
+                    return expectedValues.containsAll((Collection<?>) modelChangeDTO.newValue());
+                } else {
+                    return false;
+                }
+            } else {
+                if (modelChangeDTO.oldValue() instanceof List) {
+                    return expectedValues.containsAll((Collection<?>) modelChangeDTO.oldValue());
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    public class ArrayCondition extends Condition<ModelChangeDTO> {
+        private final Object[] expectedValues;
+        private final boolean onNewValue;
+
+        public ArrayCondition(Object[] expectedValues, boolean onNewValue) {
+            this.expectedValues = expectedValues;
+            this.onNewValue = onNewValue;
+        }
+
+        @Override
+        public boolean matches(ModelChangeDTO modelChangeDTO) {
+            Object[] arrTest = null;
+            if (onNewValue) {
+                if (isArray(modelChangeDTO.newValue())) {
+                    arrTest = convertToObjectArray(modelChangeDTO.newValue());
+                }
+            } else {
+                if (isArray(modelChangeDTO.oldValue())) {
+                    arrTest = convertToObjectArray(modelChangeDTO.oldValue());
+                }
+            }
+            if (arrTest == null) {
+                return false;
+            } else {
+                return Arrays.equals(arrTest, expectedValues);
+            }
+        }
+
+        public boolean isArray(Object obj) {
+            return obj != null && obj.getClass().isArray();
+        }
+
+        public Object[] convertToObjectArray(Object array) {
+            if (!isArray(array)) {
+                throw new IllegalArgumentException("The provided object is not an array");
+            }
+
+            int length = java.lang.reflect.Array.getLength(array);
+            Object[] outputArray = new Object[length];
+            for (int i = 0; i < length; i++) {
+                outputArray[i] = java.lang.reflect.Array.get(array, i);
+            }
+            return outputArray;
+        }
     }
 }
